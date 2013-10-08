8d2d0075a8f95b4659463f70c28a50f5001bcdad
Enrique Tejera <alu0100537512@ull.edu.es>
2013-10-08 16:04:42 +0100

    suma completado

---
+++ suma.rb
@@ -0,0 +1,16 @@
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+b = [[2, 3, 4], [5, 6, 7], [8, 9, 0]]
+c = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+if a.length == b.length && a[0].length == b[0].length
+	for i in 0...a.length
+		for j in 0...b[0].length
+			c[i][j] = a[i][j] + b[i][j]	
+		end
+	end
+	c.each do |fila|
+		puts fila.join(" ")
+	end
+end
+
+
