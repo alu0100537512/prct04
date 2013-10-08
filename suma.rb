86fabfd3d991d0dfaf6d0859f59c771978217a63
Enrique Tejera <alu0100537512@ull.edu.es>
2013-10-08 16:35:43 +0100

    suma modificada

--- suma.rb
+++ suma.rb
@@ -1,3 +1,4 @@
+# Declaramos los array
 a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 b = [[2, 3, 4], [5, 6, 7], [8, 9, 0]]
 c = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] #Inicializada matriz para almacenar resultados
@@ -5,11 +6,11 @@ c = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] #Inicializada matriz para almacenar result
 if a.length == b.length && a[0].length == b[0].length
 	for i in 0...a.length
 		for j in 0...b[0].length
-			c[i][j] = a[i][j] + b[i][j]	
+			c[i][j] = a[i][j] + b[i][j]	#Esta es la operacion
 		end
 	end
 	c.each do |fila|
-		puts fila.join(" ")
+		puts fila.join(" ") # Esto lo muestra correctamente
 	end
 end
 
