d219c43f7c3666e7541dbda633cb38657a930bd3
Hector Ravelo <alu0100603170@ull.edu.es>
2013-10-08 16:49:31 +0100

    producto.rb finalizado y comentado

--- producto.rb
+++ producto.rb
@@ -1,3 +1,4 @@
+# Inicializando matrices
 a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 b = [[2, 3, 4], [5, 6, 7], [8, 9, 0]]
 c = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -8,13 +9,13 @@ if a[0].length == b.length
 		for j in 0...b[0].length
 			x = 0
 			for k in 0...b[0].length
-				x += a[i][k] * b[k][j]
+				x += a[i][k] * b[k][j] # Se realiza la operación
 			end
 			c[i][j] = x
 		end
 	end
 	c.each do |fila|
-		puts fila.join(" ")
+		puts fila.join(" ") # Se le da formato a la matriz
 	end
 		
 end
